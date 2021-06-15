428357c894994e978618eb73f3e70db3f97eb36b
luizcfb <luizcfb@yahoo.com.br>
2021-06-15 15:33:06 -0300

    Final login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -16,14 +16,14 @@ class _login_screenState extends State<login_screen> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-      child: Column(children: <Widget>[
-        Container(
-          height: 750,
-          child: Container(
-              child: Stack(children: <Widget>[
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Container(
+              height: 750,
+              child: Container(
+                  child: Stack(children: <Widget>[
                 Positioned(
                     child: Container(
                   decoration: BoxDecoration(
@@ -34,96 +34,159 @@ class _login_screenState extends State<login_screen> {
                 )),
                 Positioned(
                     child: Align(
-                      alignment: Alignment.topCenter,
-                       child: Container(
-                         margin: const EdgeInsets.only(top: 15.0),
-                         height: 150,
-                         width: 150,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 15.0),
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
                             image: AssetImage('assets/image/logo.colorida.png'),
                             fit: BoxFit.cover,
-
                           )),
-                    ))),
-                 Padding(
+                        ))),
+                Padding(
                   padding: EdgeInsets.symmetric(horizontal: 60, vertical: 150),
                   child: Column(
                     children: <Widget>[
-                      Text("Login", style: TextStyle(color: Colors.white, fontSize: 20))
+                      Text("Login",
+                          style: TextStyle(color: Colors.white, fontSize: 20))
                     ],
                   ),
                 ),
                 Container(
                   height: 50,
                   margin: EdgeInsets.only(top: 180, left: 40, right: 40),
-                  padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                  padding:
+                      EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30)
-                  ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
                   child: TextField(
                     decoration: InputDecoration(
-                      icon: Icon(Icons.email),
-                      hintText: "E-mail",
+                        icon: Icon(Icons.email),
+                        hintText: "E-mail",
                         enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                        )
-                    ),
+                            borderSide: BorderSide(color: Colors.white))),
                   ),
                 ),
 
                 Container(
                   height: 50,
                   margin: EdgeInsets.only(top: 250, left: 40, right: 40),
-                  padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                  padding:
+                      EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
                   decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(30)
-                  ),
+                      borderRadius: BorderRadius.circular(30)),
                   child: TextFormField(
                     decoration: InputDecoration(
-                        icon: Icon(Icons.lock_rounded),
-                        hintText: "Senha",
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white)
-                        ),
+                      icon: Icon(Icons.lock_rounded),
+                      hintText: "Senha",
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
                     ),
                     obscureText: true,
                   ),
                 ),
-
                 Container(
-                  margin: EdgeInsets.only(top: 320, left: 40, right: 40),
-                  padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
-                  child: SizedBox(
-                    height: 50,
-                    width: 700,
-                  child: RaisedButton(
-                    onPressed: (){},
-                    color: Color(0xFF9f1a1b),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30))
-                    ),
-                    child: Text(
-                      "Login", style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  )),
-
-                  Container(
-                      margin: EdgeInsets.only(top: 370),
-                      padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                    margin: EdgeInsets.only(top: 320, left: 40, right: 40),
+                    padding:
+                        EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                    child: SizedBox(
                       height: 50,
                       width: 700,
-                  child: TextButton(
-                  child: Text('Esqueceu Sua Senha?', style: TextStyle(color: Colors.white),),
-                  onPressed: (){},
-    )),
-              ])),
-        ),
+                      child: RaisedButton(
+                        onPressed: () {},
+                        color: Color(0xFF9f1a1b),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    )),
+                Container(
+                    margin: EdgeInsets.only(top: 375),
+                    padding:
+                        EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                    alignment: Alignment.center,
+                    height: 50,
+                   // width: 700,
+                    child: TextButton(
+                      child: Text(
+                        'Esqueceu Sua Senha?',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    )),
 
-      ]),
-      ));
+                    Container(
+                        margin: EdgeInsets.only(top: 500),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                        height: 50,
+                          child: Text(
+                            'Não Tem Uma Conta?',
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                        ),
+
+                    Container(
+                        margin: EdgeInsets.only(top: 560, left: 40, right: 40),
+                        padding:
+                        EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                        child: SizedBox(
+                          height: 50,
+                          width: 700,
+                          child: RaisedButton(
+                            onPressed: () {},
+                            color: Color(0xFF9f1a1b),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                            child: Text(
+                              "Cadastre-se",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        )),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 630, right: 5, left: 5),
+                            child: IconButton(
+                               icon: Image.asset('assets/image/fb.png'),
+                               iconSize: 30,
+                               onPressed: () {},
+                             ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 630, right: 5, left: 5),
+                          child: IconButton(
+                            icon: Image.asset('assets/image/google.png'),
+                            iconSize: 30,
+                            onPressed: () {},
+                          ),
+                        ),
+
+                        Container(
+                          margin: EdgeInsets.only(top: 640, right: 5, left: 5),
+                          child: IconButton(
+                            icon: Image.asset('assets/image/twitter.png'),
+                            iconSize: 30,
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+              ])),
+            ),
+          ]),
+        ));
   }
 }
