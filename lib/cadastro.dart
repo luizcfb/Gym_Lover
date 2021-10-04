c02adf9d6d5bf0e4f779f1d76df87435bbd19531
luizcfb <luizcfb@yahoo.com.br>
2021-10-03 23:32:45 -0300

    Merge branch 'main' of https://github.com/luizcfb/Gym_Lover into main
    # Conflicts:
    #	.flutter-plugins-dependencies

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,4 +1,6 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class cadastro_screen extends StatefulWidget {
   @override
@@ -8,6 +10,52 @@ class cadastro_screen extends StatefulWidget {
 class _cadastro_screenState extends State<cadastro_screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final altura = MediaQuery.of(context).size.height;
+    final largura = MediaQuery.of(context).size.width;
+
+    return DefaultTabController(
+        length: 2,
+        child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/image/background_cadastro.jpg'),
+                    fit: BoxFit.cover)),
+            child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: PreferredSize(
+                    preferredSize: Size.fromHeight(75.0),
+                    child: AppBar(
+                        backgroundColor: Color(0x44000000),
+                        elevation: 0.0,
+                        bottom: TabBar(
+                          indicatorColor: Colors.white,
+                          labelStyle: TextStyle(fontSize: 20.00),
+                          tabs: [
+                            Tab(text: 'Sou Aluno'),
+                            Tab(text: 'Sou Instrutor'),
+                          ],
+                        ))),
+                body: TabBarView(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: altura * 0.04),
+                        child: Positioned(
+                            child: Align(
+                          alignment: Alignment.topCenter,
+                          child: CircleAvatar(
+                              radius: largura / 4,
+                              backgroundColor:
+                                  Color(0xFFd0312b).withOpacity(0.95),
+                              child: Icon(
+                                FontAwesomeIcons.user,
+                                color: Colors.white,
+                                size: altura * 0.12,
+                              )),
+                        ))
+                    ),
+//--------------------------------------------------------------------------//
+                    Container()
+                  ],
+                ))));
   }
 }
